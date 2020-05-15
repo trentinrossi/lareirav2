66a5cdf821a3d67c888c7ddbcf522838f417df44
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-05-15 10:10:04 -0300

    Criação e Configuração do Projeto

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Acessar o H2
+http://localhost:8080/h2-console
---
+++ src/test/java/br/com/lareira/lareiraapinelio/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lareira.lareiraapinelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
